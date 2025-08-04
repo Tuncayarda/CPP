ce9c6086d50ad1edbbf4d9d56507f25baca75f56
Tuncay Arda AYDIN <tncyard@gmail.com>
2025-08-04 03:47:53 +0300

    ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,8 +1,17 @@
 #include "Account.hpp"
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
 Account::Account( int initial_deposit)
 {
-	
+	_amount = initial_deposit;
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
 }
 
 int	Account::getNbAccounts() { return (_nbAccounts); }
