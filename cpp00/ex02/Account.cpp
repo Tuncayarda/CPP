9741c710c811ba705dfe3d3c745f9a97591cba1c
Tuncay Arda AYDIN <tncyard@gmail.com>
2025-08-04 17:24:22 +0300

    ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
 #include "Account.hpp"
 
 int Account::_nbAccounts = 0;
@@ -5,13 +8,30 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
+Account::Account()
+{
+
+}
+
 Account::Account( int initial_deposit)
 {
 	_amount = initial_deposit;
+	_totalAmount += initial_deposit;
 	_accountIndex = _nbAccounts;
 	_nbAccounts++;
 	_nbDeposits = 0;
 	_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";amount:" << _amount
+		<< ";created"
+		<< std::endl;
+}
+
+Account::~Account()
+{
+
 }
 
 int	Account::getNbAccounts() { return (_nbAccounts); }
@@ -19,3 +39,34 @@ int	Account::getTotalAmount() { return (_totalAmount); }
 int	Account::getNbDeposits() { return (_totalNbDeposits); }
 int	Account::getNbWithdrawals() { return (_totalNbWithdrawals); }
 
+void	Account::_displayTimestamp( void )
+{
+	std::time_t	now = std::time(nullptr);
+	std::tm*	tm = std::localtime(&now);
+	std::cout << "[" << std::put_time(tm, "%Y%m%d_%H%M%S") << "] ";
+}
+
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout
+	<< "accounts:" << _nbAccounts
+	<< ";total:" << _totalAmount
+	<< ";deposits:" << _totalNbDeposits
+	<< ";withdrawals:" << _totalNbWithdrawals
+	<< std::endl;
+}
+void	Account::makeDeposit( int deposit ) {}
+bool	Account::makeWithdrawal ( int withdrawal ) {return true;}
+int		Account::checkAmount() const {return 31;}
+
+void	Account::displayStatus() const
+{
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";amount:" << _amount
+		<< ";deposits:" << _nbDeposits
+		<< ";withdrawals" << _nbWithdrawals
+		<< std::endl;
+}
