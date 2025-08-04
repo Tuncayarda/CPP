f82d547fbd894fd29594fe476ce89df4c60921a1
Tuncay Arda AYDIN <tncyard@gmail.com>
2025-08-04 18:59:14 +0300

    ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -10,7 +10,11 @@ int Account::_totalNbWithdrawals = 0;
 
 Account::Account()
 {
-
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
 }
 
 Account::Account( int initial_deposit)
@@ -31,7 +35,11 @@ Account::Account( int initial_deposit)
 
 Account::~Account()
 {
-
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";amount:" << _amount
+		<< ";closed" << std::endl;
 }
 
 int	Account::getNbAccounts() { return (_nbAccounts); }
@@ -50,15 +58,55 @@ void	Account::displayAccountsInfos()
 {
 	_displayTimestamp();
 	std::cout
-	<< "accounts:" << _nbAccounts
-	<< ";total:" << _totalAmount
-	<< ";deposits:" << _totalNbDeposits
-	<< ";withdrawals:" << _totalNbWithdrawals
-	<< std::endl;
+		<< "accounts:" << _nbAccounts
+		<< ";total:" << _totalAmount
+		<< ";deposits:" << _totalNbDeposits
+		<< ";withdrawals:" << _totalNbWithdrawals
+		<< std::endl;
 }
-void	Account::makeDeposit( int deposit ) {}
-bool	Account::makeWithdrawal ( int withdrawal ) {return true;}
-int		Account::checkAmount() const {return 31;}
+void	Account::makeDeposit( int deposit )
+{
+	_amount += deposit;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+	_nbDeposits++;
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";p_amount:" << _amount - deposit
+		<< ";deposit:" << deposit
+		<< ";amount:" << _amount
+		<< ";nb_deposits:" << _nbDeposits
+		<< std::endl;
+}
+
+bool	Account::makeWithdrawal ( int withdrawal )
+{
+	if (withdrawal > _amount)
+	{
+		_displayTimestamp();
+		std::cout
+			<< "index:" << _accountIndex
+			<< ";p_amount:" << _amount
+			<< ";withdrawal:refused" << std::endl;
+		return (false);
+	}
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	_displayTimestamp();
+	std::cout
+			<< "index:" << _accountIndex
+			<< ";p_amount:" << _amount + withdrawal
+			<< ";withdrawal:" << withdrawal
+			<< ";amount:" << _amount
+			<< ";nb_withdrawals:" << _nbWithdrawals
+			<< std::endl;
+	return (true);
+}
+
+int		Account::checkAmount() const { return (_amount); }
 
 void	Account::displayStatus() const
 {
@@ -67,6 +115,6 @@ void	Account::displayStatus() const
 		<< "index:" << _accountIndex
 		<< ";amount:" << _amount
 		<< ";deposits:" << _nbDeposits
-		<< ";withdrawals" << _nbWithdrawals
+		<< ";withdrawals:" << _nbWithdrawals
 		<< std::endl;
 }
