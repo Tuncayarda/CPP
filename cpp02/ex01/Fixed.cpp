6b5a50f1e092f746c89eb8d75efe2e826831096a
Tuncayarda <tncyard@gmail.com>
2026-02-24 19:55:03 +0300

    bug fix

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "Fixed.hpp"
 
 Fixed::Fixed() : raw(0)
@@ -14,7 +15,7 @@ Fixed::Fixed( const int val )
 Fixed::Fixed( const float val )
 {
     std::cout << "Float constructor called" << std::endl;
-    raw = roundf(val * 256);
+    raw = roundf(val * (1 << frac));
 }
 
 Fixed::~Fixed()
--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "Fixed.hpp"
 
 Fixed::Fixed() : raw(0)
